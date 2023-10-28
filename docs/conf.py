4e5e939c6b53bdd3ae7b4614dcd98da21e0fdf57
Antonio Valentino <antonio.valentino@tiscali.it>
2023-10-28 17:41:27 +0200

    Silence sphinx warnings

--- docs/conf.py
+++ docs/conf.py
@@ -12,10 +12,11 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath(".."))
 
 
-def get_version(filename='../geomodels/_version.py', strip_extra=False):
+def get_version(filename="../geomodels/_version.py", strip_extra=False):
     import re
     import packaging.version
 
@@ -24,9 +25,11 @@ def get_version(filename='../geomodels/_version.py', strip_extra=False):
 
     mobj = re.search(
         r"""^__version__\s*=\s*(?P<quote>['"])(?P<version>.*)(?P=quote)""",
-        data, re.MULTILINE)
-    
-    version = packaging.version.parse(mobj.group('version'))
+        data,
+        re.MULTILINE,
+    )
+
+    version = packaging.version.parse(mobj.group("version"))
 
     if strip_extra:
         return version.base_version
@@ -36,9 +39,9 @@ def get_version(filename='../geomodels/_version.py', strip_extra=False):
 
 # -- Project information -----------------------------------------------------
 
-project = 'GeoModels'
-copyright = '2019-2023, Antonio Valentino'
-author = 'Antonio Valentino'
+project = "GeoModels"
+copyright = "2019-2023, Antonio Valentino"
+author = "Antonio Valentino"
 
 # The short X.Y version.
 version = get_version(strip_extra=True)
@@ -53,27 +56,27 @@ release = get_version(strip_extra=False)
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.imgmath',
-    'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.extlinks',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.imgmath",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.extlinks",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -81,7 +84,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -90,17 +93,17 @@ html_theme_options = {
     # Service links and badges¶
     # 'badge_branch': 'master',  # not supported by Sphinx v1.8.5
     # 'codecov_button': True,
-    'github_user': 'avalentino',
-    'github_repo': 'geomodels',
-    'github_banner': True,
-    'github_button': True,
-    'github_type': 'watch',  # 'watch', 'fork', 'follow'
-    'github_count': True,
+    "github_user": "avalentino",
+    "github_repo": "geomodels",
+    "github_banner": True,
+    "github_button": True,
+    "github_type": "watch",  # 'watch', 'fork', 'follow'
+    "github_count": True,
     # 'travis_button': True,
     # Non-service sidebar control
-    'extra_nav_links': {
-        'GeoModels on PyPI': 'https://pypi.org/project/geomodels',
-        'GeoGraphicLib': 'https://geographiclib.sourceforge.io/html',
+    "extra_nav_links": {
+        "GeoModels on PyPI": "https://pypi.org/project/geomodels",
+        "GeoGraphicLib": "https://geographiclib.sourceforge.io/html",
     },
     # 'show_related': True,
     # 'sidebar_collapse': True,  # not supported by Sphinx v1.8.5
@@ -112,7 +115,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -120,11 +123,11 @@ html_static_path = ['_static']
 # This is required for the alabaster theme
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
 html_sidebars = {
-    '**': [
-        'about.html',
-        'navigation.html',
-        'relations.html',
-        'searchbox.html',
+    "**": [
+        "about.html",
+        "navigation.html",
+        "relations.html",
+        "searchbox.html",
     ],
 }
 
@@ -133,9 +136,9 @@ html_sidebars = {
 
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
-    'papersize': 'a4paper',
+    "papersize": "a4paper",
     # The font size ('10pt', '11pt' or '12pt').
-    'pointsize': '12pt',
+    "pointsize": "12pt",
     # Additional stuff for the LaTeX preamble.
     # 'preamble': '',
     # Latex figure (float) alignment
@@ -146,8 +149,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'GeoModels.tex', 'GeoModels Documentation',
-     author, 'manual'),
+    (master_doc, "GeoModels.tex", "GeoModels Documentation", author, "manual"),
 ]
 
 # If false, no module index is generated.
@@ -160,10 +162,10 @@ latex_domain_indices = False
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3', None),
-              # '/usr/share/doc/python3/html/objects.inv'),
-    'numpy':  ('https://docs.scipy.org/doc/numpy', None),
-              # '/usr/share/doc/python-numpy-doc/html/objects.inv'),
+    "python": ("https://docs.python.org/3", None),
+    # '/usr/share/doc/python3/html/objects.inv'),
+    "numpy": ("https://docs.scipy.org/doc/numpy", None),
+    # '/usr/share/doc/python-numpy-doc/html/objects.inv'),
 }
 
 
@@ -171,5 +173,5 @@ intersphinx_mapping = {
 
 # External links configuration
 extlinks = {
-    'issue': ('https://github.com/avalentino/geomodels/issues/%s', 'gh-'),
+    "issue": ("https://github.com/avalentino/geomodels/issues/%s", "gh-%s"),
 }
